b85b0b0651c42e358db83ddd07cbfc382ff65040
Manoj Mehta <mmehta4@sapient.com>
2019-08-30 00:11:33 -0400

    added babel loader

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,7 +2,9 @@ const path = require("path");
 
 module.exports = {
     entry: {
-        main: ["./src/main.js"]
+        main: [
+            "./src/main.js"
+        ]
     },
     mode:"development",
     output: {
@@ -11,6 +13,62 @@ module.exports = {
         publicPath: "/"
     },
     devServer: {
-        contentBase: "dist"
+        contentBase: "dist",
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].html"
+                        }
+                    },
+                    {
+                        loader: "extract-loader"
+                    },
+                    {
+                        loader: "html-loader",
+                        options: {
+                            attrs: ["img:src"]
+                        }
+                    }
+                ]
+            },
+            {
+                test:/\.(jpg|gif|png)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "images/[name]-[hash:8].[ext]"
+                        }
+                    }
+                ]
+            }
+        ]
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-alert('hello world')
+require("babel-runtime/regenerator");
+require('./main.css');
+require("./index.html");
+
+var a = (async (args) => {
+    const {a, b} = args;
+    await console.log("arrow functions", a, b);
+    console.log("done");
+});
+a({a: 1, b: 2});
